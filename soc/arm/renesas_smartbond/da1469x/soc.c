ef1714064f5a6dbad61459156867fd277fc507ce
Ioannis Karachalios <ioannis.karachalios.px@renesas.com>
2023-06-13 07:06:00 -0700

    soc: smartbond: da1469x: virtual to physical translation
    Support converting virtual to physical addresses required by
    peripherals.
    
    Signed-off-by: Ioannis Karachalios <ioannis.karachalios.px@renesas.com>

--- soc/arm/renesas_smartbond/da1469x/soc.c
+++ soc/arm/renesas_smartbond/da1469x/soc.c
@@ -27,6 +27,14 @@
 #define FLASH_REGION_SIZE_05M     6
 #define FLASH_REGION_SIZE_025M    7
 
+enum REMAP_ADDR0 {
+	REMAP_ADDR0_ROM = 0x0,
+	REMAP_ADDR0_OTP,
+	REMAP_ADDR0_QSPIF_CACHED,
+	REMAP_ADDR0_SYSRAM,
+	REMAP_ADDR0_MAX,
+};
+
 #if defined(CONFIG_BOOTLOADER_MCUBOOT)
 #define MAGIC 0xaabbccdd
 static uint32_t z_renesas_cache_configured;
@@ -112,6 +120,63 @@ void z_arm_platform_init(void)
 #if defined(CONFIG_BOOTLOADER_MCUBOOT)
 	z_renesas_configure_cache();
 #endif
+}
+
+uint32_t black_orca_phy_addr(uint32_t addr)
+{
+	uint32_t remap_addr0;
+	uint32_t phy_addr;
+
+	static const uint32_t remap_bases[] = {
+		[REMAP_ADDR0_ROM] = MCU_ROM_BASE,
+		[REMAP_ADDR0_OTP] = MCU_OTP_M_BASE,
+		[REMAP_ADDR0_QSPIF_CACHED] = MCU_QSPIF_M_CACHED_BASE,
+		[REMAP_ADDR0_SYSRAM] = MCU_SYSRAM_M_BASE
+	};
+
+	static const uint32_t flash_region_sizes[] = {
+		MB(32),
+		MB(16),
+		MB(8),
+		MB(4),
+		MB(2),
+		MB(1),
+		KB(512),
+		KB(256)
+	};
+
+	remap_addr0 = CRG_TOP->SYS_CTRL_REG & CRG_TOP_SYS_CTRL_REG_REMAP_ADR0_Msk;
+	assert(remap_addr0 < REMAP_ADDR0_MAX);
+
+	if (remap_addr0 != REMAP_ADDR0_QSPIF_CACHED) {
+		if (IS_REMAPPED_ADDRESS(addr)) {
+			phy_addr = addr + remap_bases[remap_addr0];
+		} else {
+			phy_addr = addr;
+		}
+	} else {
+		/* FLASH_REGION_BASE corresponds to Flash region base address bits [31:16] */
+		uint32_t flash_region_base = ((CACHE->CACHE_FLASH_REG & CACHE_CACHE_FLASH_REG_FLASH_REGION_BASE_Msk) >>
+																CACHE_CACHE_FLASH_REG_FLASH_REGION_BASE_Pos) << 16;
+		/* FLASH_REGION_OFFSET corresponds to Flash region base address bits [13:2]. Offset is expressed in words. */
+		flash_region_base += ((CACHE->CACHE_FLASH_REG & CACHE_CACHE_FLASH_REG_FLASH_REGION_OFFSET_Msk) >>
+														CACHE_CACHE_FLASH_REG_FLASH_REGION_OFFSET_Pos) << 2;
+		__unused uint32_t flash_region_size = flash_region_sizes[CACHE->CACHE_FLASH_REG & CACHE_CACHE_FLASH_REG_FLASH_REGION_SIZE_Msk];
+
+		if (IS_REMAPPED_ADDRESS(addr)) {
+			assert(addr < flash_region_size);
+
+			phy_addr = addr + flash_region_base;
+		} else if (IS_QSPIF_CACHED_ADDRESS(addr)) {
+			assert(addr >= flash_region_base && addr < (flash_region_base + flash_region_size));
+
+			phy_addr = addr;
+		} else {
+			phy_addr = addr;
+		}
+	}
+
+	return phy_addr;
 }
 
 static int renesas_da1469x_init(void)
